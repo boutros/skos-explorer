b224d8f2f790ca0f0b7aeb76a567ffd3338539d5
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-02-27 13:51:00 +0100

    small refactoring

--- src/clj/skos_explorer/views.clj
+++ src/clj/skos_explorer/views.clj
@@ -12,18 +12,21 @@
   "public/skos.html"
   [uri bindings narrower broader related topconcepts]
 
-  [:h2] (if (some #(= (str uri) %) (map :concept topconcepts))
-          (html/content (str "Top concept: " (->> bindings :preflabel first)))
-          (html/content (str "Concept: " (->> bindings :preflabel first))))
+  [:h2] (html/content (str
+          (if (some #(= (str uri) %) (map :concept topconcepts))
+            "Top concept: "
+            "Concept: ")
+            (->> bindings :preflabel first)))
   [:#uri] (html/content (str uri))
   [:#updated] (html/content (->> bindings :modified first))
   [:#comment] (html/content (->> bindings :comment first))
   [:#scope] (html/content (->> bindings :scope first))
   [:ul.narrower :li] (links narrower :narrower :narrowerlabel)
   [:ul.related :li] (links related :related :relatedlabel)
-  [:#broader-or-top] (if (some #(= (str uri) %) (map :concept topconcepts))
-                       (html/content "other top concepts")
-                       (html/content "broader concepts"))
+  [:#broader-or-top] (html/content
+                       (if (some #(= (str uri) %) (map :concept topconcepts))
+                         "other top concepts"
+                         "broader concepts"))
   [:ul.broader :li] (if (some #(= (str uri) %) (map :concept topconcepts))
                       (links (remove #(= (str uri) (:concept %)) topconcepts)
                              :concept :label)
