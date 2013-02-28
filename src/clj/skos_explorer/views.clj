85b9b72f12645dd51c5e0c61580e99e3662755f9
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-02-28 10:24:56 +0100

    show minus button to remove concepts

--- src/clj/skos_explorer/views.clj
+++ src/clj/skos_explorer/views.clj
@@ -5,8 +5,10 @@
   [linkmap uri label]
   (html/clone-for [n linkmap]
     (html/content
-      {:tag :a, :attrs {:href (str "/skos/" (re-find #"[0-9]*$"(uri n)))},
-       :content [(label n)]})))
+      [{:tag :a, :attrs {:href (str "/skos/" (re-find #"[0-9]*$"(uri n)))},
+       :content [(label n)]}
+      {:tag :div :attrs {:class "circle minus"}
+       :content [{:tag :p :content "-"}]}])))
 
 (html/deftemplate concept
   "public/skos.html"
@@ -21,23 +23,23 @@
   [:#updated] (html/content (->> bindings :modified first))
   [:#comment] (html/content (->> bindings :comment first))
   [:#scope] (html/content (->> bindings :scope first))
-  [:ul.narrower :li.first] (links narrower :narrower :narrowerlabel)
-  [:ul.related :li.first] (links related :related :relatedlabel)
+  [:ul.narrower :li.concept] (links narrower :narrower :narrowerlabel)
+  [:ul.related :li.concept] (links related :related :relatedlabel)
   [:#broader-or-top] (html/content
                        (if (some #(= (str uri) %) (map :concept topconcepts))
                          "Other top concepts"
                          "Broader concepts"))
-  [:ul.broader :li.first] (if (some #(= (str uri) %) (map :concept topconcepts))
+  [:ul.broader :li.concept] (if (some #(= (str uri) %) (map :concept topconcepts))
                       (links (remove #(= (str uri) (:concept %)) topconcepts)
                              :concept :label)
                       (links broader :broader :broaderlabel))
-  [:#alternate :li.first] (html/clone-for [n (bindings :altlabel)]
+  [:#alternate :li.concept] (html/clone-for [n (bindings :altlabel)]
                                     (html/content n))
-  [:#hidden :li.first] (html/clone-for [n (bindings :hiddenlabel)]
+  [:#hidden :li.concept] (html/clone-for [n (bindings :hiddenlabel)]
                                     (html/content n))
-  [:#related :li.first] (html/clone-for [n (bindings :relatedlabel)]
+  [:#related :li.concept] (html/clone-for [n (bindings :relatedlabel)]
                                     (html/content n))
-  [:#prefered :li.first] (html/content (->> bindings :preflabel first))
+  [:#prefered :li.concept] (html/content (->> bindings :preflabel first))
   [:.links :p.first] (html/clone-for [n (bindings :link)]
                                (html/content
                                  {:tag :a, :attrs {:href n}, :content [n]})))
