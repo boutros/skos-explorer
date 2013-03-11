11249794653f2e2c07fe47cd74a55990b256bc1b
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-03-11 13:50:33 +0100

    wip live-listener fn

--- src/cljs/skos_explorer/clientside.cljs
+++ src/cljs/skos_explorer/clientside.cljs
@@ -5,7 +5,7 @@
   (:require-macros [dommy.core-compile :refer [sel sel1]]
                    [dommy.template-compile :refer [node deftemplate]]))
 
-(repl/connect "http://localhost:9000/repl")
+;(repl/connect "http://localhost:9000/repl")
 
 (defn log [& more]
   (.log js/console (apply str more)))
@@ -16,6 +16,19 @@
   (let [event (.createEvent js/document "MouseEvents")]
     (.initMouseEvent event "click" true true)
     (.dispatchEvent node event)))
+
+(defn live-listen!
+  "Listen on all elements in container, currently elem can only be a tag"
+  [container elem event-type f]
+  (.addEventListener
+    (.querySelector js/document container)
+    (name event-type)
+    (fn [evt]
+      (when (= elem (->> evt .-target .-tagName .toLowerCase))
+        (f evt))
+    false))) ; capture
+
+(defn fu [evt] (.log js/console (->> evt .-target .-parentElement .-parentElement)))
 
 (defn ^:export init []
   (log "Hallo der, mister Åsen."))
